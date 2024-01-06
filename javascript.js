fba2a8e20dc9954c59c33f9350caceb41cd222d1
renny5120 <renny5120@gmail.com>
2024-01-05 21:47:36 -0800

    implement game()

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+
+
+// Random Choice for Computer
 function getComputerChoice(){
     const choices = [];
     choices.push("rock", "paper", "scissors");
@@ -5,4 +8,60 @@ function getComputerChoice(){
     return choices[randomElement];
 }
 
-console.log(getComputerChoice());
+// Plays Round
+function playRound(playerChoice, compChoice){
+    playerChoice = playerChoice.toLowerCase();
+    let outcome = false;
+    let tie = false;
+    if (playerChoice == "rock"){
+        if(compChoice == "scissors"){
+            outcome = true;
+        } else if (compChoice == "paper"){
+            outcome = false;
+        }else {
+            tie = true;
+        }
+    } else if (playerChoice == "paper"){
+        if(compChoice == "rock"){
+            outcome = true;
+        } else if (compChoice == "scissors"){
+            outcome = false;
+        }else {
+            tie = true;
+        }
+    } else {
+        if(compChoice == "paper"){
+            outcome = true;
+        } else if (compChoice == "rock"){
+            outcome = false;
+        }else {
+            tie = true;
+        }
+    }
+    if (tie){
+        return "tie";
+    }
+
+    if (outcome){
+        return `"You Win! ${playerChoice} beats ${compChoice}"`
+    } else {
+        return `"You Lose! ${compChoice} beats ${playerChoice}"`
+    }
+}
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let outcome = "tie";
+        while (outcome == "tie"){
+            let playerSelection = prompt("Input your Choice!");
+            outcome = playRound(playerSelection, getComputerChoice());
+        }
+        console.log(outcome);
+    }
+}
+game();
